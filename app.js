0b1c4815eefb2555207cf2aebec6d1252353c9bd
Jigar Bhavsar <jigar.rbhavsar@gmail.com>
2024-05-15 03:44:39 -0400

    set up main app.js for now

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+require('dotenv').config({path: path.resolve(__dirname, '.env')});
+const {MongoClient, ServerApiVersion} = require('mongodb');
+
+
+
+
+const portNum = process.env.PORT||3000;
+
+const uri = process.env.MONGO_CONNECTION_String;
+const dbPlusCollection = { db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION}
+
+let db;
+MongoClient.connect(uri, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+}, (err,client) => {
+    if(err) throw err;
+    db = client.db(dbPlusCollection.db);
+    console.log("Connection done to db")
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const routers = require('./routes/index')(db);
+app.use('/', routers);
+
+app.listen(portNum, () => {
+    console.log('Server running');
+})
