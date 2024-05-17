3f092a96d81a250fcc38773a63cf2e7818856f37
Jigar Bhavsar <jigar.rbhavsar@gmail.com>
2024-05-17 16:15:01 -0400

    changed it Views

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const viewsPath = path.resolve(__dirname, 'views');
+const viewsPath = path.resolve(__dirname, 'Views');
 app.set('views', viewsPath);
 app.set('view engine', 'ejs');
 
